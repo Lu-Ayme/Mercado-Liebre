923b3e1990fd04e506dd9623639989afe91c8aaf
Lu-Ayme <aymeandrade2006@gmail.com>
2023-08-13 21:57:07 -0300

    Mi último commit

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const app=express()
 
 app.use(express.static('public'))
 
-app.listen(3030, ()=>{
+/*app.listen(3030, ()=>{
     console.log('Puerto 3030 corriendo')
-})
+})*/
+const port =process.env.PORT || 3000;
+app.listen (port, ()=>console.log(`servidor Coriendo en el puerto ${port}`))
 
 app.get('/', function(req, res){
     res.sendFile(path.resolve(__dirname, './views/index.html'))
